0c1c2caf4475f7a589fa98aa5f46a8f212334d88
victorHL99 <victor.lessa@estudante.ufla.br>
2022-05-11 17:52:19 -0300

    router rodando

---
+++ controllers/loginControllers.js
@@ -0,0 +1,42 @@
+import db from '../db.js';
+import joi from 'joi';
+
+export async function postLogin(req, res) {
+    console.log("entrando")
+    const { email, password } = req.body;
+
+    const user = {
+        email,
+        senha
+    }
+
+    const userSchema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().alphanum().required()
+    });
+
+    const validateUser = userSchema.validate(user, {abortEarly: false});
+
+    if(validateUser.error){
+        res.status(422).send(validateUser.error.details.map(description => description.message));
+        return;
+    }
+
+    try{
+        const validateEmail = await db.collection("clients").findOne({email:email});
+        console.log(validateEmail);
+        if(validateEmail && bcrypt.compareSync(password, validateEmail.password)){
+            sessionStart = await db.collection("sessions").findOne({email: email});
+            console.log(sessionStart);
+            res.status(200).send(sessionStart.tokenSession);
+        } else {
+        res.status(404).send("Usuário não encontrado");
+        return;
+        }
+    }
+    catch {
+        res.status(500).send("Erro no servidor");
+    }
+
+
+}
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express, {json} from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-import db from './db.js'
+import loginRouter from './route/loginRouter.js';
+
 const app = express();
 
 
@@ -10,41 +11,6 @@ dotenv.config();
 app.use(cors());
 app.use(json());
 
-app.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-
-    const user = {
-        email,
-        senha
-    }
-
-    const userSchema = joi.object({
-        email: joi.string().email().required(),
-        password: joi.string().alphanum().required()
-    });
-
-    const validateUser = userSchema.validate(user, {abortEarly: false});
-
-    if(validateUser.error){
-        res.status(422).send(validateUser.error.details.map(description => description.message));
-        return;
-    }
-
-    try{
-        const validateEmail = await db.collection("clients").findOne({email});
-        if(validateEmail && bcrypt.compareSync(password, validateEmail.password)){
-            await {tokenSession} = await db.collection("sessions").findOne({email});
-            res.status(200).send({tokenSession});
-        } else {
-        res.status(404).send("Usuário não encontrado");
-        return;
-        }
-    }
-    catch {
-        res.status(500).send("Erro no servidor");
-    }
-
-})
 
 
 const port = process.env.PORT || 5000;
@@ -54,3 +20,5 @@ app.listen(port,()=>{
 });
 
 
+
+app.use(loginRouter);
