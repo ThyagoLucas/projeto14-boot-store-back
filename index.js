00a200dee7ba09cd0b6d9c596a42093d0df972bb
victorHL99 <victor.lessa@estudante.ufla.br>
2022-05-11 16:26:32 -0300

    Funcionalidade login escopo principal pronto

--- index.js
+++ index.js
@@ -3,18 +3,54 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import db from './db.js'
+const app = express();
+
 
 dotenv.config();
-
-const app = express();
 app.use(cors());
-app.use(json)
+app.use(json());
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = {
+        email,
+        senha
+    }
+
+    const userSchema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().alphanum().required()
+    });
+
+    const validateUser = userSchema.validate(user, {abortEarly: false});
+
+    if(validateUser.error){
+        res.status(422).send(validateUser.error.details.map(description => description.message));
+        return;
+    }
+
+    try{
+        const validateEmail = await db.collection("clients").findOne({email});
+        if(validateEmail && bcrypt.compareSync(password, validateEmail.password)){
+            await {tokenSession} = await db.collection("sessions").findOne({email});
+            res.status(200).send({tokenSession});
+        } else {
+        res.status(404).send("Usuário não encontrado");
+        return;
+        }
+    }
+    catch {
+        res.status(500).send("Erro no servidor");
+    }
+
+})
+
 
 const port = process.env.PORT || 5000;
 
 app.listen(port,()=>{
     console.log(`Servidor rodando na porta ${port}`)
 });
-
 
 
