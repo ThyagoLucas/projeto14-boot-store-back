b23860e951a85a7c4dca86d46caed0b3c410b146
Thyago Lucas <Salc@icomp.ufam.edu.br>
2022-05-12 23:10:18 -0400

    rota e controller de Buscar item finalizadas

--- route/loginRouter.js
+++ route/loginRouter.js
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 
-const loginRouter = Router();
 import {postLogin} from '.././controllers/loginControllers.js';
 
+const loginRouter = Router();
+
 loginRouter.post('/login', postLogin )
-console.log("funcionando chefe")
+
+
 
 export default loginRouter;
